f136469f78aab2744d7067cceb82693a89d59a31
Cesar Lachira <lachiracesar@gmail.com>
2018-09-23 22:04:18 -0500

    HomePagePrototype ready

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, App } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  app_name = 'OpenDayGram';
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController,
+              private app:App) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  salir(){
+    const confirm = this.alertCtrl.create({
+      title: this.app_name,
+      message: `¿Desea salir de ${this.app_name}?`,
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Salir',
+          handler: () => {
+            this.app.getRootNav().setRoot(LoginPage);
+          }
+        }
+      ]
+    });
+    confirm.present();
+    
   }
 
 }
